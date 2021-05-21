c32e66332f9ca439cab45899fa48a0bd3b66884e
AlpinePascia <al.pascia@gmail.com>
2021-05-21 12:51:34 +0300

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js🤪!!!</a>
+          Welcome to <a href="https://nextjs.org">Next.js🤪!!?</a>
         </h1>
 
         <p className={styles.description}>
